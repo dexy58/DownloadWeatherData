c4b1909f5c284c7b4e519fb99e4d179e400c2151
unknown <ivan.kralj@fer.hr>
2021-12-13 11:55:24 +0100

    Added auto date and readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # DownloadWeatherData
+
+Things TODO
+- Add Location input parameter for Main
+
+How to use
+- Run every 24 hours (to download latest data)
--- src/main/java/hr/fer/tel/Main.java
+++ src/main/java/hr/fer/tel/Main.java
@@ -6,13 +6,19 @@ import java.net.*;
 import java.io.*;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Main {
-    public static void main(String[] args) { //add input parameters for lat, lon, start, stop date
+    public static void main(String[] args) { //add input parameters for location (selected lan and lon from this input parameter)
         URL website = null;
+        String yesterday = getYesterdayDateString();
+
         try {
-            String url = String.format("https://worldmodel.csiro.au/gclimate?lat=%s&lon=%s&format=apsim&start=20210101&stop=20210201",
-                    Locations.ZAGREB_LAT, Locations.ZAGREB_LON); //date format: yyyyMMdd
+            String url = String.format("https://worldmodel.csiro.au/gclimate?lat=%s&lon=%s&format=apsim&start=%s0101&stop=%s",
+                    Locations.ZAGREB_LAT, Locations.ZAGREB_LON, getYear(0), yesterday); //date format: yyyyMMdd
             website = new URL(url);
             ReadableByteChannel rbc = Channels.newChannel(website.openStream());
             FileOutputStream fos = new FileOutputStream("C:\\text.met"); //change text file depending on location (npr. Zagreb.met if Zagreb is selected).
@@ -23,5 +29,22 @@ public class Main {
          } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static int getYear(int index) {
+        Calendar year = Calendar.getInstance();
+        year.add(Calendar.YEAR, index);
+        return year.get(Calendar.YEAR);
+    }
+
+    private static Date yesterday() {
+        final Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, -1);
+        return cal.getTime();
+    }
+
+    private static String getYesterdayDateString() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+        return dateFormat.format(yesterday());
     }
 }
